d16307c8893cf4b309f8ea0334cf5291361fd038
murilopsc <contato.murilopsc@gmail.com>
2025-02-18 19:24:45 -0300

    Codewars_training

---
+++ Jenny's_secret_message.py
@@ -0,0 +1,23 @@
+'''Jenny has written a function that returns a greeting for a user. 
+However, she's in love with Johnny, and would like to greet him 
+slightly different. 
+She added a special case to her function, but she made a mistake.
+
+Can you help her?
+
+def greet(name):
+    return "Hello, {name}!".format(name=name)
+    if name == "Johnny":
+        return "Hello, my love!"
+
+'''
+
+# My solution
+
+def greet(name):
+   
+   if name == 'Johnny':
+    return 'Hello, my love!'
+   else:
+    return "Hello, {name}!".format(name=name)
+
